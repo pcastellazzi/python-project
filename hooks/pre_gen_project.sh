71c46941aefe5cd5a1f10972820f75b39f8f4125
Pablo Castellazzi <pablo.castellazzi@icloud.com>
2023-05-11 09:34:14 -0300

    add pre-hook to check for dependencies

---
+++ hooks/pre_gen_project.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+DEPENDENCIES=(
+    make
+    poetry
+    tox
+)
+
+for dependency in "${DEPENDENCIES[@]}"; do
+    if ! type -p "${dependency}" >/dev/null; then
+        echo >&2 "ERROR: command ${dependency} is missing"
+        exit 1
+    fi
+done
